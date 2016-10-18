6659fea2a5769be7b94bc2e7f9d94ca06b988e7b
Michael Dahl Løvås <michael-dahl-lovaas@hotmail.com>
2016-10-18 22:37:52 +0200

    Update jsvvd.js

--- jsvvd.js
+++ jsvvd.js
@@ -5,11 +5,41 @@ window.onload = oppstart;
     document.getElementById("omOss").onclick = omOss;
     document.getElementById("forBarn").onclick = forBarn;
     document.getElementById("kontaktOss").onclick = kontaktOss;
+    document.getElementById("ForAmur").onclick = amur;
+    document.getElementById("ForUtryd").onclick = utryd;
+    document.getElementById("ForForest").onclick = natur;
+    document.getElementById("ForEnergi").onclick = energi;
+    document.getElementById("ForKlima").onclick = klima;
+    document.getElementById("ForHav").onclick = hav;
     
   }
 
   function banner() {
     window.location.href = "index.html"
+  }
+
+function amur() {
+    window.location.href = "amurleopard.html"
+  }
+
+function utryd() {
+    window.location.href = "index.html"
+  }
+
+function natur() {
+    window.location.href = "natur.html"
+  }
+
+function energi() {
+    window.location.href = "energi.html"
+  }
+
+function klima() {
+    window.location.href = "klima.html"
+  }
+
+function hav() {
+    window.location.href = "havogkyst.html"
   }
 
   function openNav() {
