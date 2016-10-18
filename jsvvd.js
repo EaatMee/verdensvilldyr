4a671b301d0e03e952476938bfac600ea46dd6ec
Michael Dahl Løvås <michael-dahl-lovaas@hotmail.com>
2016-10-18 18:27:09 +0200

    Update jsvvd.js

--- jsvvd.js
+++ jsvvd.js
@@ -1,1 +1,17 @@
+window.onload = oppstart;
 
+  function oppstart() {
+    document.getElementById("banner").onclick = banner;
+  }
+
+  function banner() {
+    openNav()
+  }
+
+  function openNav() {
+      document.getElementById("myNav").style.height = "100%";
+  }
+
+  function closeNav() {
+      document.getElementById("myNav").style.height = "0%";
+  }
